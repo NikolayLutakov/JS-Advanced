9272fde4bb7e5e3ba6bad0c5437de5b303394817
Nikolay Lyutakov <nikolaylutakov@gmail.com>
2021-09-25 13:44:26 +0300

    DOM Introduction Exercise solutions added.

--- DOM Introduction/08. Generate Report/report.js
+++ DOM Introduction/08. Generate Report/report.js
@@ -1,3 +1,16 @@
 function generateReport() {
-    //TODO
+    const choices = Array.from(document.querySelectorAll('thead tr th'));
+    const information = Array.from(document.querySelectorAll('tbody tr'))
+    const allInfo = [];
+
+    for (const row of information) {
+        const rowInfo = {};
+        for (let i = 0; i < choices.length; i++) {
+            if (choices[i].childNodes[1].checked) {
+                rowInfo[choices[i].textContent.toLowerCase().trimEnd()] = row.children[i].innerHTML;
+            }
+        }
+        allInfo.push(rowInfo)
+    }
+    document.getElementById('output').value = JSON.stringify(allInfo, null, 2);
 }
--- DOM Introduction/09. Number Convertor/solution.js
+++ DOM Introduction/09. Number Convertor/solution.js
@@ -1,4 +1,33 @@
 function solve() {
+    const selectTable = document.getElementById('selectMenuTo');
+    const binary = selectTable.querySelector('option');
+    binary.textContent = 'Binary';
+    binary.value = 'binary';
+    const hexadecimal = document.createElement('option');
+    hexadecimal.textContent = 'Hexadecimal';
+    hexadecimal.value = 'hexadecimal';
+    selectTable.appendChild(hexadecimal);
 
-    //TODO...
+    function onClick() {
+        const input = Number(document.getElementById('input').value);
+        const toSelected = document.getElementById('selectMenuTo').value;
+        const functions = {
+            binary: binaryConvert,
+            hexadecimal: hexadecimalConvert
+        }
+        if (toSelected != '') {
+            document.getElementById('result').value = functions[toSelected](input);
+        }
+
+        function binaryConvert(number) {
+            binaryString = number.toString(2);
+            return binaryString;
+        }
+
+        function hexadecimalConvert(number) {
+            hexString = number.toString(16).toUpperCase();
+            return hexString;
+        }
+    }
+    document.querySelector('button').addEventListener('click', onClick);
 }
