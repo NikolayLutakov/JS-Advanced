4b85ee4f130d9b88348dabe14b6ae02a5a159da9
Nikolay Lyutakov <nikolaylutakov@gmail.com>
2021-11-09 12:45:28 +0200

    Task 05 started.

--- Data and Authentication/05.Fisher-Game/src/login.js
+++ Data and Authentication/05.Fisher-Game/src/login.js
@@ -1,1 +1,25 @@
-console.log('TODO:// Implement Login functionality');
+function attachEvents() {
+    document.getElementById('loginForm').addEventListener('submit', makeLogin)
+}
+
+function makeLogin(e) {
+    const notification = document.querySelector('.notification');
+    notification.textContent = '';
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+    const email = formData.get('email');
+    const password = formData.get('password');
+    if(email == '' || password == ''){
+        notification.textContent = 'Email and Password fields are required';
+        return;
+    }
+
+    //make request
+
+    //save token
+
+    //redirect to home
+}
+
+window.onload = attachEvents;
